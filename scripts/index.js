5c95cadddcda38e585159477f4210ff32251c033
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2022-12-09 15:27:08 +0300

    add JS submit

--- scripts/index.js
+++ scripts/index.js
@@ -19,11 +19,47 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
+// popup.addEventListener('click', function(event){
+//   if(!event.defaultPrevented) {
+//     closePopup();
+//   }
+// })
+// document.querySelector('.popup__container').addEventListener('click', function(event){
+//   event.preventDefault();
+// })
+
 popup.addEventListener('click', function(event){
-  if(!event.defaultPrevented) {
+  if(event.target === event.currentTarget){
     closePopup();
   }
-})
-document.querySelector('.popup__container').addEventListener('click', function(event){
-  event.preventDefault();
-})
+});
+
+
+// делаем сабмит
+
+let formElement = document.querySelector('#popup-form')
+
+console.log(formElement)
+let nameInput = document.querySelector('#input-name')
+let jobInput = document.querySelector('#input-job')
+
+
+
+function handleFormSubmit(evt) {
+    console.log(123)
+    evt.preventDefault();
+    let nameValue = nameInput.value
+    let jobValue = jobInput.value
+
+
+  let profileName = document.querySelector('.Profile__name')
+  let profileJob = document.querySelector('.Profile__aboute')
+
+    profileName.textContent = nameValue
+    profileJob.textContent = jobValue
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
