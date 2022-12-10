acd183392c18d9800a0bee410ade3eed65662e86
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2022-12-10 17:10:57 +0300

    Finish develop

--- scripts/index.js
+++ scripts/index.js
@@ -19,14 +19,6 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-// popup.addEventListener('click', function(event){
-//   if(!event.defaultPrevented) {
-//     closePopup();
-//   }
-// })
-// document.querySelector('.popup__container').addEventListener('click', function(event){
-//   event.preventDefault();
-// })
 
 popup.addEventListener('click', function(event){
   if(event.target === event.currentTarget){
@@ -46,20 +38,17 @@ let jobInput = document.querySelector('#input-job')
 
 
 function handleFormSubmit(evt) {
-    console.log(123)
-    evt.preventDefault();
-    let nameValue = nameInput.value
-    let jobValue = jobInput.value
-
+  evt.preventDefault();
+  let nameValue = nameInput.value
+  let jobValue = jobInput.value
 
   let profileName = document.querySelector('.Profile__name')
   let profileJob = document.querySelector('.Profile__aboute')
 
-    profileName.textContent = nameValue
-    profileJob.textContent = jobValue
-
+  profileName.textContent = nameValue
+  profileJob.textContent = jobValue
+  closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
-
 
