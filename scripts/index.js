944cd36824e9d7369bedbb1c80b8a8e5fa229219
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2022-12-12 19:31:07 +0300

    v1.2

--- scripts/index.js
+++ scripts/index.js
@@ -2,38 +2,36 @@
 //.popup
 //.popup__close
 
-const profileEdit = document.querySelector('.Profile__edit');
+const profileEdit = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 
+let formElement = document.querySelector('#popup-form')
+let nameInput = formElement.querySelector('#input-name')
+let jobInput = formElement.querySelector('#input-job')
+let profileInfo = document.querySelector('.profile__info')
+let profileName = profileInfo.querySelector('.profile__name')
+let profileJob = profileInfo.querySelector('.profile__aboute')
 
 profileEdit.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 
+
 function openPopup(event) {
-  event.preventDefault();
   popup.classList.add('popup_opened')
+  nameInput.value = profileName.innerHTML;
+  jobInput.value = profileJob.innerHTML;
 }
+
+
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
 
-popup.addEventListener('click', function(event){
-  if(event.target === event.currentTarget){
-    closePopup();
-  }
-});
-
 
 // делаем сабмит
-
-let formElement = document.querySelector('#popup-form')
-
-console.log(formElement)
-let nameInput = document.querySelector('#input-name')
-let jobInput = document.querySelector('#input-job')
 
 
 
@@ -41,10 +39,6 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   let nameValue = nameInput.value
   let jobValue = jobInput.value
-
-  let profileName = document.querySelector('.Profile__name')
-  let profileJob = document.querySelector('.Profile__aboute')
-
   profileName.textContent = nameValue
   profileJob.textContent = jobValue
   closePopup();
@@ -52,3 +46,5 @@ function handleFormSubmit(evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 // здесь сделать кнопку лайк
+
+
