8fcfb2c45ad37b96be1aa66a9708df27058caaa8
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2022-12-26 19:49:03 +0300

    Добавили карточки в JS

--- scripts/index.js
+++ scripts/index.js
@@ -46,3 +46,43 @@ formElement.addEventListener('submit', handleFormSubmit);
 // здесь сделать кнопку лайк
 
 
+const initialCards = [
+  {
+    name: 'Космонавт',
+    link: '../images/CCCP.png'
+  },
+  {
+    name: 'Глинтвейн',
+    link: '../images/orange-juice.png'
+  },
+  {
+    name: 'Пианист',
+    link: '../images/piano.png'
+  },
+  {
+    name: 'Карачаевск',
+    link: '../images/photo1.png'
+  },
+  {
+    name: 'Вкуснотища!',
+    link: '../images/pizza.png'
+  },
+  {
+    name: 'Фотограф',
+    link: './images/photograph.jpg'
+  }
+];
+
+
+initialCards.forEach((card) => {
+
+  const element = `<article class="element">
+    <img src="${card.link}" alt="Карачаевск" class="element__photo">
+    <div class="element__photo-info">
+    <h2 class="element__title">${card.name}</h2>
+    <button type="button" class="element__button-like"></button>
+    </div>
+  </article>`
+  const cardContainer = document.querySelector('.elements');
+  cardContainer.insertAdjacentHTML('afterbegin', element)
+});
