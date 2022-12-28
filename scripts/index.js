64425addc384e40a1c7d092c6d318013f1982197
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2022-12-28 16:31:44 +0300

    Карточки
    Сделал добавление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,25 @@
 //.Profile__edit
 //.popup
 //.popup__close
-
+//Переменные для попапа
 const profileEdit = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
+
+//Переменные для карточек
+// const addCards = document.querySelector('.profile__add-button');
+// const popupCards = document.querySelector('.popup_cards');
+// const popupCardsClose = document.querySelector('.popup_cards_close');
+// // const formCard = document.querySelector('#popup-form-card');
+// // const nameCard = formElement.querySelector('#card-name');
+// // const urlCard = formElement.querySelector('#card-url');
+// addCards.addEventListener('click', openCardPopup);
+// // popupCardsClose.addEventListener('click', closeCardPopup);
+
+
+
+
+
 
 const formElement = document.querySelector('#popup-form')
 const nameInput = formElement.querySelector('#input-name')
@@ -27,11 +42,7 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-
-
 // делаем сабмит
-
-
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -46,43 +57,138 @@ formElement.addEventListener('submit', handleFormSubmit);
 // здесь сделать кнопку лайк
 
 
-const initialCards = [
-  {
-    name: 'Космонавт',
-    link: '../images/CCCP.png'
-  },
-  {
-    name: 'Глинтвейн',
-    link: '../images/orange-juice.png'
-  },
-  {
-    name: 'Пианист',
-    link: '../images/piano.png'
-  },
-  {
-    name: 'Карачаевск',
-    link: '../images/photo1.png'
-  },
-  {
-    name: 'Вкуснотища!',
-    link: '../images/pizza.png'
-  },
-  {
-    name: 'Фотограф',
-    link: './images/photograph.jpg'
-  }
-];
 
 
-initialCards.forEach((card) => {
 
-  const element = `<article class="element">
-    <img src="${card.link}" alt="Карачаевск" class="element__photo">
-    <div class="element__photo-info">
-    <h2 class="element__title">${card.name}</h2>
-    <button type="button" class="element__button-like"></button>
-    </div>
-  </article>`
-  const cardContainer = document.querySelector('.elements');
-  cardContainer.insertAdjacentHTML('afterbegin', element)
+
+// надо сделать, чтобы картинки добавлялись в начало
+// const initialCards = [
+//   {
+//     name: 'Карачаевск',
+//     link: '../images/photo1.png'
+//   },
+//   {
+//     name: 'Глинтвейн',
+//     link: '../images/orange-juice.png'
+//   },
+//   {
+//     name: 'Пианист',
+//     link: '../images/piano.png'
+//   },
+//   {
+//     name: 'Космонавт',
+//     link: '../images/CCCP.png'
+//   },
+//   {
+//     name: 'Вкуснотища!',
+//     link: '../images/pizza.png'
+//   },
+//   {
+//     name: 'Фотограф',
+//     link: './images/photograph.jpg'
+//   }
+// ];
+
+// initialCards.forEach((card) => {
+//   const element = `<article class="element">
+//     <img src="${card.link}" alt="Карачаевск" class="element__photo">
+//     <div class="element__photo-info">
+//     <h2 class="element__title">${card.name}</h2>
+//     <button type="button" class="element__button-like"></button>
+//     </div>
+//   </article>`
+//   const cardContainer = document.querySelector('.elements');
+//   cardContainer.insertAdjacentHTML('afterbegin', element)
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+//пробуем метод из вебинара
+
+const cards =  [
+    {
+      name: 'Карачаевск',
+      link: '../images/photo1.png'
+    },
+    {
+      name: 'Глинтвейн',
+      link: '../images/orange-juice.png'
+    },
+    {
+      name: 'Пианист',
+      link: '../images/piano.png'
+    },
+    {
+      name: 'Космонавт',
+      link: '../images/CCCP.png'
+    },
+    {
+      name: 'Вкуснотища!',
+      link: '../images/pizza.png'
+    },
+    {
+      name: 'Фотограф',
+      link: './images/photograph.jpg'
+    }
+  ];
+
+const cardContainer = document.querySelector('.elements');
+const createCard = (card) => {
+  return `<article class="element">
+      <img src="${card.link}" alt="Карачаевск" class="element__photo">
+       <div class="element__photo-info">
+       <h2 class="element__title">${card.name}</h2>
+      <button type="button" class="element__button-like"></button>
+      </div>
+    </article>`
+}
+
+
+
+const renderCard = (card) => {
+  cardContainer.insertAdjacentHTML('afterbegin', createCard(card))
+}
+
+cards.forEach((card) => {
+  renderCard(card);
+})
+
+
+//ВЫВОД: КАК В ВЕБИНАРЕ НЕ ПОЛУЧАЕТСЯ!
+
+const openCardButton = document.querySelector('.profile__add-button');
+const popupCards = document.querySelector('.popup_cards');
+const titleInput = popupCards.querySelector('#card-name');
+const linkInput = popupCards.querySelector('#card-link');
+const submitButton = popupCards.querySelector('#submit-button-card');
+
+
+
+
+openCardButton.addEventListener('click', () => {
+  popupCards.classList.add('popup_opened')
 });
+
+submitButton.addEventListener('click',(e) => {
+  e.preventDefault();
+  const newCard = {
+    name: titleInput.value,
+    link: linkInput.value
+  }
+
+  if(!newCard.name || !newCard.link) return;
+
+  renderCard(newCard)
+  popupCards.classList.remove('popup_opened');
+})
+
+
