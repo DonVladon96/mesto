349caed9569f78dd0dcf4a4c6f7aa3af87b9f269
DonVladon96 <Petrov.Vladik2010@yandex.ru>
2023-01-06 16:21:43 +0300

    rew2

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,12 @@
 //Переменные для попапа
 import { cards } from "./constants.js";
 
-const profileEdit = document.querySelector('.profile__edit');
+const buttonOpenPopupEditProfile = document.querySelector('.profile__edit');
 const popupEditProfile = document.querySelector('.popup_edit-profile');
-const popupCloseProfile = document.querySelector('.popup__close');
-const formElement = document.querySelector('#popup-form')
-const nameInput = formElement.querySelector('#input-name')
-const jobInput = formElement.querySelector('#input-job')
+const buttonClosePopupEditProfile = document.querySelector('.popup__close');
+const popupFormEdit = document.querySelector('#popup-form')
+const nameInput = popupFormEdit.querySelector('#input-name')
+const jobInput = popupFormEdit.querySelector('#input-job')
 const profileInfo = document.querySelector('.profile__info')
 const profileName = profileInfo.querySelector('.profile__name')
 const profileJob = profileInfo.querySelector('.profile__aboute')
@@ -23,8 +23,8 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-profileEdit.addEventListener('click', openProfilePopup);
-popupCloseProfile.addEventListener('click', () => {
+buttonOpenPopupEditProfile.addEventListener('click', openProfilePopup);
+buttonClosePopupEditProfile.addEventListener('click', () => {
   closePopup(popupEditProfile)
 });
 
@@ -34,10 +34,9 @@ function openProfilePopup(event) {
   jobInput.value = profileJob.textContent;
 }
 
-
 // делаем сабмит
 
-function handleFormSubmit(evt) {
+function submitEditProfileForm(evt) {
   evt.preventDefault();
   const nameValue = nameInput.value
   const jobValue = jobInput.value
@@ -46,30 +45,33 @@ function handleFormSubmit(evt) {
   closePopup(popupEditProfile);
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+popupFormEdit.addEventListener('submit', submitEditProfileForm);
 
 
 const template = document.querySelector('#card-item-template');
-
 const cardContainer = document.querySelector('.elements');
+const popupImageOpen = document.querySelector('.popup_image-open')
+const popupImage = popupImageOpen.querySelector('.popup__image')
+const popupCaption = popupImageOpen.querySelector('.popup__caption')
+
 const createCard = (card) => {
   const container = template.content.querySelector('.element').cloneNode(true);
   const likeButton = container.querySelector('.element__button-like');
   const buttonTrash = container.querySelector('.element__button-trash');
-  const openCard = container.querySelector('.element__photo');
-  openCard.setAttribute('src', card.link)
 
+  const cardImage = container.querySelector('.element__photo');
+  cardImage.setAttribute('src', card.link)
+  cardImage.setAttribute('alt', card.name)
   const cardTitle = container.querySelector('.element__title');
   cardTitle.textContent = card.name;
 
-  const popupImageOpen = document.querySelector('.popup_image-open')
-  const popupImage = popupImageOpen.querySelector('.popup__image')
-  const popupCaption = popupImageOpen.querySelector('.popup__caption')
-  openCard.addEventListener('click', () => {
+
+  cardImage.addEventListener('click', () => {
 
     popupImage.setAttribute('src', card.link)
+    popupImage.setAttribute('alt', card.name)
     popupCaption.textContent = card.name;
-    // popupImageOpen.classList.add('popup_opened')
+
     openPopup(popupImageOpen);
   });
 
@@ -83,11 +85,9 @@ const createCard = (card) => {
   return container;
 }
 
-
 // Кнопка закрытия картинки
-const popupImageOpen = document.querySelector('.popup_image-open')
-const popupCloseImg = document.querySelector('.popup__close-image');
-popupCloseImg.addEventListener('click', () => {
+const buttonClosePopupImage = document.querySelector('.popup__close-image');
+buttonClosePopupImage.addEventListener('click', () => {
   closePopup(popupImageOpen)
 })
 
@@ -102,21 +102,21 @@ cards.forEach((card) => {
 //ВЫВОД: СДЕЛАЛ КАК В ВЕБИНАРЕ.
 // Делаем лайк кнопки <button type="button" class="element__button-like"></button>
 // .element__button-like_active
-const openCardButton = document.querySelector('.profile__add-button');
+const buttonOpenPopupAddCard = document.querySelector('.profile__add-button');
 const popupCards = document.querySelector('.popup-cards');
 const titleInput = popupCards.querySelector('#card-name');
 const linkInput = popupCards.querySelector('#card-link');
 const submitButton = popupCards.querySelector('#submit-button-card');
 
 //кнопка открытия редактора карточек
-openCardButton.addEventListener('click', () => {
+buttonOpenPopupAddCard.addEventListener('click', () => {
   // popupCards.classList.add('popup_opened')
   openPopup(popupCards)
 });
 
 // Кнопка закрытия редактора карточек
-const cardClose = document.querySelector('.popup__cards-close')
-cardClose.addEventListener('click', () => {
+const buttonClosePopupAddCard = document.querySelector('.popup__cards-close')
+buttonClosePopupAddCard.addEventListener('click', () => {
   closePopup(popupCards)
 })
 
